8af25a69714bc5319028c288ff9cdd56176e1bd0
Thibaut Durand <durand.tibo@gmail.com>
2026-08-23 14:36:48 -0700

    Set up repo

---
+++ src/plotmux/__init__.py
@@ -0,0 +1,11 @@
+r"""Top-level package for ``plotmux``."""
+
+__all__ = ["__version__"]
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    # Package is not installed, fallback if needed
+    __version__ = "0.0.0"
